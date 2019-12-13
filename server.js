d172f221c849551c7a0e5cb9e9af475b8f0d4248
msoldo1 <marins1277@gmail.com>
2019-12-13 23:47:00 +0100

    initial author routes

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express'); //ubacivanje expresa iz libery
 const app = express();
 const expressLayouts = require('express-ejs-layouts');//dodavanje dodatnog paketa
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs')//kreiranje pogleda
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{
@@ -24,6 +27,8 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
+app.listen(process.env.PORT|| 3000);
 
