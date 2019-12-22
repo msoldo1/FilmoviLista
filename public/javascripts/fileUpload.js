436ecca82d35137fefd13532345364dec0635646
msoldo1 <marins1277@gmail.com>
2019-12-23 00:46:03 +0100

    Zavrsen Projekt

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,29 @@
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') != '' ){
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load',ready)
+}
 
-FilePond.parse(document.body);
+function ready(){
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'));
+    const coverAspectRation = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ration'));
+    const coverHeight = coverWidth / coverAspectRation;
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRation ,
+        imageResizeTargetWidth: 100,
+        imageResizeTargetHeight: 150
+    })
+    
+    FilePond.parse(document.body);
+
+}
+
 
